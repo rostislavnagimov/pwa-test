89eb65cd35cb976d7466648860e026fd867db702
Rostislav Nagimov <rostislav.nagimov@gmail.com>
2024-02-13 21:30:04 -0800

    feat: implement the PWA

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,45 @@
-import Image from "next/image";
+"use client"
+import React, { useEffect, useState } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const [deferredPrompt, setDeferredPrompt] = useState<any>(null);
+
+  useEffect(() => {
+    const handleBeforeInstallPrompt = (e: any) => {
+      e.preventDefault();
+      setDeferredPrompt(e);
+    };
+
+    window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
+
+    return () => {
+      window.removeEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
+    };
+  }, []);
+
+  const handleInstallButtonClick = () => {
+    if (deferredPrompt) {
+      deferredPrompt.prompt();
+
+      deferredPrompt.userChoice.then((choiceResult: { outcome: string }) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the install prompt');
+        } else {
+          console.log('User dismissed the install prompt');
+        }
+      });
+
+      setDeferredPrompt(null);
+    }
+  };
+
   return (
     <main className={styles.main}>
-      <button className={styles.button}>
+      <button className={styles.button} onClick={handleInstallButtonClick}>
         Install
       </button>
+      <iframe src="https://www.wikipedia.org/" className={styles.iframe}></iframe>
     </main>
   );
 }
---
+++ next.config.js
@@ -0,0 +1,6 @@
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
+  dest: 'public',
+  // disable: process.env.NODE_ENV === 'development',
+});
